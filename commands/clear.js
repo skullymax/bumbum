a0430dd923d01d35c0a841065e8c6fe0d36e74c3
skullymax <38359146+skullymax@users.noreply.github.com>
2018-05-01 23:11:59 +0200

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,16 @@
+module.exports = {
+     main: function(bot, msg, role) {
+     const deleteCount = parseInt(args[0], 10);
+
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+		});
+    },
+    help: 'Clear chat'
+};
