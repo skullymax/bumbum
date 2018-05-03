860e515b6cb41e8d9b754d038287bb3decb62f88
skullymax <38359146+skullymax@users.noreply.github.com>
2018-05-03 20:36:32 +0200

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -5,7 +5,7 @@ module.exports = {
 		msg.channel.send("Thanks for using EyzAltsFREE").then(function(newMsg) {
 			var stop = Date.now();
 			var diff = (stop - start);
-			msg.channel.send("https://discord.gg/GMpmh");
+			msg.channel.send("https://discord.gg/VAwQKQG");
 		});
     },
     help: 'Join to official discord'
